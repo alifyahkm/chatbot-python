4fa1ea5c647a957e09169142727309f1a5fbd620
Fya <alifyahikmah@gmail.com>
2022-08-11 08:48:22 +0700

    update view

--- chatbottf.py
+++ chatbottf.py
@@ -13,10 +13,10 @@ from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, GlobalMaxPool
 from tensorflow.keras.models import Model
 import matplotlib.pyplot as plt
 
-# importing the dataset
+# memuat dataset
 with open('content.json') as content:
     data1 = json.load(content)
-# getting all the data to lists
+# memasukkan setiap data dalam json kebentuk list
 tags = []
 inputs = []
 responses = {}
@@ -25,53 +25,53 @@ for intent in data1['intents']:
     for lines in intent['input']:
         inputs.append(lines)
         tags.append(intent['tag'])
-# converting to dataframe
+# mengubah bentuk data dari json ke dataframe pandas
 data = pd.DataFrame({"inputs": inputs,
                      "tags": tags})
 print(data)
 
-# removing punctuations
+# menghapus tanda baca
 data['inputs'] = data['inputs'].apply(
     lambda wrd: [ltrs.lower() for ltrs in wrd if ltrs not in string.punctuation])
 data['inputs'] = data['inputs'].apply(lambda wrd: ''.join(wrd))
-# tokenize the data
+# melakukan tokenizing pada data input
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(data['inputs'])
 train = tokenizer.texts_to_sequences(data['inputs'])
 
-# apply padding
+# menambahkan padding sequences pada data x_train
 x_train = pad_sequences(train)
 
-# encoding the outputs
+# melakukan encoding atau merubah tag menjadi angka
 le = LabelEncoder()
 y_train = le.fit_transform(data['tags'])
 
-# input length
+# mengambil ukuran data input
 input_shape = x_train.shape[1]
 print(input_shape)
-# define vocabulary
+# mendefinisikan kosa kata yang unik
 vocabulary = len(tokenizer.word_index)
 print("number of unique words : ", vocabulary)
-# output length
+# mengambil ukuran data output
 output_length = le.classes_.shape[0]
 print("output length: ", output_length)
 
-# creating the model
+# membentuk model neural network
 i = Input(shape=(input_shape,))
 x = Embedding(vocabulary+1, 10)(i)
 x = LSTM(10, return_sequences=True)(x)
 x = Flatten()(x)
 x = Dense(output_length, activation="softmax")(x)
 model = Model(i, x)
-# compiling the model
+# melakukan kompilasi pada model
 model.compile(loss="sparse_categorical_crossentropy",
               optimizer='adam', metrics=['accuracy'])
-# training the model
+# melakukan training dengan model yang sudah dibentuk
 train = model.fit(x_train, y_train, epochs=500, batch_size=25, verbose=1)
 
 
 def response_to_chat(prediction_input):
- # removing punctuation and converting to lowercase
+ # menghapus tanda baca dan mengubah ke huruf kecil semua
     texts_p = []
     prediction_input = [letters.lower(
     ) for letters in prediction_input if letters not in string.punctuation]
@@ -81,12 +81,12 @@ def response_to_chat(prediction_input):
     prediction_input = tokenizer.texts_to_sequences(texts_p)
     prediction_input = np.array(prediction_input).reshape(-1)
     prediction_input = pad_sequences([prediction_input], input_shape)
-    # getting output from model
+    # mengambil output dari model yang sudah di train
     output = model.predict(prediction_input)
     print(f"Predict :{output}")
     output = output.argmax()
     print(f"Argmax:{output}")
-    # finding the right tag and predicting
+    # mencari tag sesuai hasil dari model
     response_tag = le.inverse_transform([output])[0]
     return random.choice(responses[response_tag])
 
